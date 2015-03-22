ae436c8057d774f8819e64346e65c76be55089ea
JB Braendel <Jbud@live.ca>
2015-03-22 16:07:03 -0700

    added registers & reg editor

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-define(['canvas'], function(Canvas){
+define(['UI', 'canvas'], function(UI, Canvas){
 
 	Keys.add(['STAGE_IF', 'STAGE_ID', 'STAGE_EX', 'STAGE_MEM', 'STAGE_WB']); // Instruction Pipeline/stages
 
+
+	// MIPS specifications
+	//
+	// Following the O32 Calling Convention found here:
+	// http://en.wikipedia.org/wiki/MIPS_instruction_set#Compiler_register_usage
+	var Specifications = {
+			registers: [
+				'zero', // constant 0
+				'at', // assembler temporary
+				'v0', 'v1', // function return values
+				'a0', 'a1', 'a2', 'a3', // function arguments
+				't0', 't1', 't2', 't3', 't4', 't5', 't6', 't7', // temporaries
+				's0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', // saved temporaries
+				't8', 't9', // temporaries
+				'k0', 'k1', // reserved for OS kernel
+				'gp', 'sp', 'fp', // global/stack/frame pointer
+				'ra' // return address
+			],
+		};
+
+
+
+	// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% //
+	//									User Experience Stuff
+	// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% //
 
 	// Startup the canvas
 	var canvas = new Canvas();
@@ -14,5 +39,10 @@ define(['canvas'], function(Canvas){
 	canvas.addComponent({ uid: 1 }, STAGE_IF);
 	canvas.addComponent({ uid: 2 }, STAGE_ID);
 	canvas.addComponent({ uid: 3 }, STAGE_EX);
+
+
+	// UI startup
+	var UI = new UI();
+	UI.initRegisters(Specifications.registers);
 
 });
